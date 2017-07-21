db77eeb955160ffe7cdb6cef8f1689f402f787b9
taozen <buptzhengtao@gmail.com>
2017-07-21 12:07:29 +0800

    Add upper bound

--- trial.py
+++ trial.py
@@ -1,10 +1,12 @@
 import random
 
-def simulate():
+def simulate(max_num):
     # Stop when we get 1, and return number of 0 before that.
     cnt = 0
     while (random.randrange(0, 2) != 1):
         cnt += 1
+        if (cnt == max_num):
+            break;
 
     return cnt
 
@@ -12,10 +14,14 @@ def trial(n_repeat):
     # Run trial once
     girls = 0
     boys = 0
+    max_num_children = 5
 
     for i in range(0, n_repeat):
-        girls += simulate()
-        boys += 1
+        cnt = simulate(max_num_children)
+        girls += cnt
+
+        if cnt < max_num_children:
+            boys += 1
 
     print "girls=", girls, ", boys=", boys, ", ratio=", float(girls)/boys
 
